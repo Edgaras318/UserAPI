fd50effc296c35f6f741780f9368e60831189b3a
edgaras318 <edgaras318@gmail.com>
2023-11-28 17:50:04 +0200

    some more unit tests

--- tests/Unit/Controllers/UserControllerTest.php
+++ tests/Unit/Controllers/UserControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserControllerTest extends TestCase
 {
@@ -28,6 +29,90 @@ class UserControllerTest extends TestCase
     $response->assertStatus(201)
       ->assertJson([
         'message' => 'User created successfully',
+      ]);
+  }
+
+  public function test_authenticated_user_can_update_own_profile()
+  {
+    // Create a user
+    $user = User::factory()->create();
+
+    // Simulate authentication by acting as the user
+    $this->actingAs($user);
+
+    // Update payload
+    $updateData = [
+      'first_name' => 'Jane',
+      'last_name' => 'Doe',
+      'email' => 'john1@example.com',
+      'password' => 'password1',
+    ];
+
+    // Send a PUT request to update the user's own profile
+    $response = $this->put("/api/users/{$user->id}", $updateData);
+
+    // Assert response status and content
+    $response->assertStatus(200)
+      ->assertJson([
+        'message' => 'User updated successfully',
+      ]);
+  }
+
+  public function test_authenticated_user_cannot_delete_own_account()
+  {
+    // Create a user
+    $user = User::factory()->create();
+
+    // Simulate authentication by acting as the user
+    $this->actingAs($user);
+
+    // Send a DELETE request to delete the user's own account
+    $response = $this->delete("/api/users/{$user->id}");
+
+    // Assert response status and content
+    $response->assertStatus(403)
+      ->assertJson([
+        'message' => 'You cannot delete your own account',
+      ]);
+  }
+  public function test_authenticated_user_can_delete_other_user_account()
+  {
+    // Create two users
+    $userA = User::factory()->create();
+    $userB = User::factory()->create();
+
+    // Simulate authentication as user A
+    $this->actingAs($userA);
+
+    // Send a DELETE request to delete user B's account
+    $response = $this->delete("/api/users/{$userB->id}");
+
+    // Assert response status and content
+    $response->assertStatus(200)
+      ->assertJson([
+        'message' => 'User deleted successfully',
+      ]);
+  }
+
+  public function test_authenticated_user_can_get_all_users()
+  {
+    // Create an authenticated user
+    $user = User::factory()->create();
+
+    // Simulate authentication by acting as the user
+    $this->actingAs($user);
+
+    // Send a GET request to retrieve all users
+    $response = $this->get('/api/users');
+
+    // Retrieve all users from the database
+    $users = User::all()->toArray();
+
+    // Assert response status and content
+    $response->assertStatus(200)
+      ->assertJson([
+        'users' => $users, // Compare directly to the retrieved users
+        // Add more assertions based on the expected response structure
       ]);
   }
 }
