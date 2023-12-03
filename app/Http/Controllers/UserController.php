2b17dab1c1ebc94e5dea254c936787a2c39910ad
edgaras318 <edgaras318@gmail.com>
2023-12-03 15:38:10 +0200

    import of db is no longer needed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Services\UserService;
